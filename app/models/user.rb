686fbe8843fb03ba1bb96003f20cff014d47b38f
Frank Torchia <ftt9282@rit.edu>
2017-03-08 22:30:16 -0500

    Add Friendship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :friendships
+  has_many :active_friends, :through => :friendships
+  has_many :passive_friends, :through => :friendships
 end
