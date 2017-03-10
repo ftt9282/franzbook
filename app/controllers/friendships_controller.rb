66294cde407ac00f3a7811b9883d93cbdee0a749
Frank Torchia <ftt9282@rit.edu>
2017-03-10 01:26:29 -0500

    Update routes to have friendships be a member of users

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,4 +1,7 @@
 class FriendshipsController < ApplicationController
+  def show
+  end
+  
   def create
   	@user = User.find(params[:friend_id])
   	current_user.send_friend_request(@user)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:index, :show]
-  resources :friendships, only: [:create, :update, :destroy]
+  resources :users, only: [:index, :show] do
+    resources :friendships, only: [:index, :create, :update, :destroy]
+  end
 end
